21afca69bf2cc4d75f19160acef1d9840e042236
TheRenato <ropazo81@gmail.com>
2019-05-22 12:52:47 +0200

    found a bug on getCurrentTax function

--- Studynomic/app/src/main/java/com/opazoweb/studynomic/data/Skatteverket/municipality/MunicipalityApi.kt
+++ Studynomic/app/src/main/java/com/opazoweb/studynomic/data/Skatteverket/municipality/MunicipalityApi.kt
@@ -1,28 +1,47 @@
 package com.opazoweb.studynomic.data.Skatteverket.municipality
 
 import androidx.room.Query
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
 import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 import java.time.Year
 
 interface MunicipalityApi {
 
-    @GET()
+    @GET("c67b320b-ffee-4876-b073-dd9236cd2a99")
     fun getCurrentTax(
-        @Query( "kommun") city: String,
-        @Query("%C3%A5r") year: Year
+        @Query("kommun") city: String,
+        @Query("år") year: Int =  2019
     ):Deferred<Result>
 
     companion object{
         operator fun invoke(): MunicipalityApi {
             val requestInterceptor = Interceptor{chain ->
 
-                val url = chain.request()
-                    .url()
+                val request = chain.request()
                     .newBuilder()
                     .build()
+
+                return@Interceptor chain.proceed(request)
             }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://skatteverket.entryscape.net/rowstore/dataset/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MunicipalityApi::class.java)
+
         }
     }
 }
--- Studynomic/app/src/main/java/com/opazoweb/studynomic/ui/summery/work/SummeryWorkFragment.kt
+++ Studynomic/app/src/main/java/com/opazoweb/studynomic/ui/summery/work/SummeryWorkFragment.kt
@@ -8,6 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.opazoweb.studynomic.R
+import com.opazoweb.studynomic.data.Skatteverket.municipality.MunicipalityApi
+import kotlinx.android.synthetic.main.summery_work_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SummeryWorkFragment : Fragment() {
 
@@ -28,6 +33,14 @@ class SummeryWorkFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SummeryWorkViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val municipalityApi = MunicipalityApi()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val municipalitySkatteverketResponse = municipalityApi.getCurrentTax(city = "KÖPING").await()
+            summeryWorkFragmentText.text = municipalitySkatteverketResponse.toString()
+
+        }
     }
 
 }
